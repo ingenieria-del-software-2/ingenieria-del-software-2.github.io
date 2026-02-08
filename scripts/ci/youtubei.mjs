a3511a46f21aadace16ca3b9cc9212620a130b70
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2026-02-08 11:44:51 -0300

    refactor(youtubei): optimize data fetching by using a for loop instead of Promise.all

--- scripts/ci/youtubei.mjs
+++ scripts/ci/youtubei.mjs
@@ -7,17 +7,19 @@ const yt = await Innertube.create();
 const text = await readFile(SRC, 'utf8');
 const ids = [...new Set([...text.matchAll(/(?:v=|shorts\/|embed\/|youtu\.be\/)([\w-]{11})/g)].map(m => m[1]))];
 
-const data = await Promise.all(ids.map(async (id) => {
+const data = [];
+for (const id of ids) {
   const { basic_info: i = {} } = await yt.getBasicInfo(id).catch(err => {
     console.error(id, err);
     return {};
   });
 
-  return [id, {
+  data.push([id, {
     title: i.title ?? null,
     duration: Number(i.duration) || null,
-    thumb: i.thumbnail?.sort((a, b) => b.width - a.width)[0]?.url ?? `https://i.ytimg.com/vi/${id}/hqdefault.jpg`  }];
-}));
+    thumb: i.thumbnail?.sort((a, b) => b.width - a.width)[0]?.url ?? `https://i.ytimg.com/vi/${id}/hqdefault.jpg`
+  }]);
+}
 
 await mkdir(dirname(DEST), { recursive: true });
 await writeFile(DEST, JSON.stringify(Object.fromEntries(data), null, 2));
